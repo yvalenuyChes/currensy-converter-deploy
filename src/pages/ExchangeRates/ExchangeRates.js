d706b18a9472ab663bcdb61bbea64f542a3877be
Vladislav_Zaporozhets <yvalenuyches@gmail.com>
2023-05-28 11:53:14 +0300

    add preloader to exchange rates page

--- src/pages/ExchangeRates/ExchangeRates.js
+++ src/pages/ExchangeRates/ExchangeRates.js
@@ -9,7 +9,7 @@ import classes from './ExchangeRates.module.scss'
 
 export default function ExchangeRates() {
 
-   const [result, setResult] = useState(null)
+   const [result, setResult] = useState(false)
 
    useEffect(() => {
       axios({
@@ -35,14 +35,12 @@ export default function ExchangeRates() {
          <h1 className={classes.title}>{navigator.language === ('ru' || 'ru-RU') ? 'Курс рубля' : 'Dollar rate'}</h1>
 
          {
+            result
+            ?
             navigator.language === ('ru' || 'ru-RU')
                ?
                <ul className={classes.rates__list}>
                   {
-                  result === null
-                  ?
-                  <Loader/>
-                  :
                   Object.entries(result).map((item, key) =>
                      <li key={key} className={classes.rates__list__item}>
                         Валюта: <span>{item[0]}</span>, рублей за валюту:<span>{item[1]}</span> руб
@@ -52,16 +50,19 @@ export default function ExchangeRates() {
                :
                <ul className={classes.rates__list}>
                   {
-                  result === null
-                  ? <Loader/>
-
-                  :
                   Object.entries(result).map(item =>
                      <li key={item[0]} className={classes.rates__list__item}>
                         Valute: <span>{item[0]}</span>, Rate:<span>{item[1].toFixed(3)}</span> for 1 euro
                      </li>
                   )}
                </ul>
+            :
+            <div style={{display:'flex', alignItems:'center', justifyContent:'center', paddingTop:'40px'}} >
+               <Loader
+                  size={'50px'}
+               />
+            </div>
+          
          }
          <div className={classes.rates__link}>
             <NavLink to='/'>{navigator.language === ('ru' || 'ru-RU') ? 'Конвертер' : 'Converter'}</NavLink>
